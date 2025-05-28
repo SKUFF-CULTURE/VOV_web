bf9fce640e45c62f7cb01efa67d202848bcfc3e2
MeL <53662166+MeLver0@users.noreply.github.com>
2025-05-28 20:25:58 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -89,7 +89,7 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 3000,
+    port: 5173,
     strictPort: true,
     // разрешить любые хосты (в режиме разработки)
     // либо явно перечислить: ['frontend', 'localhost', 'голос-победы.рф']
